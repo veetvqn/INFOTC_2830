3836c166d6b79ab8bbe8b711013315507f9cc1b4
Martin Leija <martinleija5@gmail.com>
2024-12-05 00:44:26 -0600

    Front end with photo files included

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,7 +3,18 @@ import React from 'react';
 function Dashboard() {
   return (
     <div>
-      title
+      <div class="planner-app">
+          <h2>Add New Tasks</h2>
+          <div class="row">
+            <input type="text" name="newTast" value="Add a New Task"></input>
+            <button>Add</button>
+          </div>
+          <ul id="list-container">
+            <li class="checked">Task 1</li>
+            <li>Task 2</li>
+            <li>Task 3</li>
+          </ul>
+      </div> 
     </div>
   );
 }
