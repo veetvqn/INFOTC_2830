ce041676234299dd0aa07eb4d1f4abecfe8b48da
veetvqn <vietqnagooyan@gmail.com>
2024-12-04 22:23:32 -0600

    fixed file structure and added tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      title
+    </div>
+  );
+}
+
+export default Dashboard;
